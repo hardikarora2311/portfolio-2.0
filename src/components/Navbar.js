72f5d38fa500ee063b124b8eca3346e530ca3f1f
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2022-04-26 01:48:34 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {HiMenuAlt4} from "react-icons/hi"
+import {IoMdClose} from "react-icons/io"
 
 const Navbar = () => {
+  const [show, setShow]= useState(false)
   return (
     <nav className="navbar">
         <div className="logo">PORTFOLIO</div>
-        <ul className="non-bullet nav-menu">
-            <li className="list-inline nav-items">Projects</li>
-            <li className="list-inline nav-items">Contact</li>
-        </ul>
+        <button className='nav-btn' onClick={()=> setShow(!show)}><HiMenuAlt4/></button>
+        <div className={show? "show-nav" :"nav-menu"}>
+            <div className="center-div">
+                <ul className="non-bullet nav-list">
+                    <li className="list-inline nav-items">Projects</li>
+                    <li className="list-inline nav-items">Contact</li>
+                </ul>
+            </div>
+            <button className='close-btn' onClick={()=> setShow(!show)}><IoMdClose/></button>
+        </div>
+
     </nav>
-  )
+  ) 
 }
 
 export default Navbar
